5e7f4f28a4f7061d14198e4d5df64c901ae1b13b
Gabriel Donadel <gabidonadel1@gmail.com>
2019-10-30 11:03:19 -0300

    new version

--- safe/safe.ino
+++ safe/safe.ino
@@ -6,22 +6,58 @@
 */
 
 #include <Arduino.h>
-
 #include <ESP8266WiFi.h>
 #include <ESP8266WiFiMulti.h>
-
 #include <ESP8266HTTPClient.h>
-
 #include <WiFiClient.h>
+#include <SPI.h>
+#include <MFRC522.h>
+
+
+#include <Servo.h>
+
+Servo servo;
+
+
+//Inicializa a biblioteca utilizando as portas de 8 a 11 para 
+//ligacao ao motor 
+  
+
 
 ESP8266WiFiMulti WiFiMulti;
 WiFiServer server(80);
 char user[50] = "Doni";
+unsigned long uid;
+bool isDoorOpen = false;
+
+#define RST_PIN         D3         // Configurable, see typical pin layout above
+#define SS_PIN          D8        // Configurable, see typical pin layout above
+ 
+MFRC522 mfrc522(SS_PIN, RST_PIN);  // Create MFRC522 instance
+
+unsigned long getID(){
+  if ( ! mfrc522.PICC_ReadCardSerial()) { //Since a PICC placed get Serial and continue
+    return -1;
+  }
+  unsigned long hex_num;
+  hex_num =  mfrc522.uid.uidByte[0] << 24;
+  hex_num += mfrc522.uid.uidByte[1] << 16;
+  hex_num += mfrc522.uid.uidByte[2] <<  8;
+  hex_num += mfrc522.uid.uidByte[3];
+  mfrc522.PICC_HaltA(); // Stop reading
+  return hex_num;
+}
+ 
 
 void setup() {
 
+
   Serial.begin(115200);
   // Serial.setDebugOutput(true);
+
+  SPI.begin();      // Init SPI bus
+  mfrc522.PCD_Init();   // Init MFRC522
+  mfrc522.PCD_DumpVersionToSerial();  // Show details of PCD - MFRC522 Card Reader details
 
   Serial.println();
   Serial.println();
@@ -29,12 +65,13 @@ void setup() {
 
   for (uint8_t t = 4; t > 0; t--) {
     Serial.printf("[SETUP] WAIT %d...\n", t);
+    Serial.printf("port d1 %d, port d7 %d\n", D1,D7);
     Serial.flush();
     delay(1000);
   }
 
   WiFi.mode(WIFI_STA);
-  WiFiMulti.addAP("donadel", "123456789");
+  WiFiMulti.addAP("D0N1", "abcdef1234");
 
   // Inicia o Servidor no qual iremos ver no navegador e poder acionar o Relé
   server.begin();
@@ -46,11 +83,39 @@ void setup() {
   Serial.print(WiFi.localIP());
   Serial.println("/");
 
+  servo.attach(D9);
+  servo.write(0);
 }
 
 void loop() {
+
+
+  
+
   // wait for WiFi connection
   if ((WiFiMulti.run() == WL_CONNECTED)) {
+    // Look for new cards
+    if (mfrc522.PICC_IsNewCardPresent()) { 
+        uid = getID();
+        if(uid != -1){
+          Serial.print("Card detected, UID: "); Serial.println(uid); 
+          
+           //Gira o motor no sentido anti-horario a 120 graus
+        if(isDoorOpen){
+          servo.write(0);
+          }
+         else{
+          servo.write(180);
+          }
+
+          isDoorOpen = !isDoorOpen;
+   
+          Serial.print((String)"Door is Open "+isDoorOpen);
+        } 
+    
+ 
+    }
+ 
  
     WiFiClient client = server.available();
     if (!client) {
@@ -62,8 +127,17 @@ void loop() {
     client.println("<!DOCTYPE HTML>");
     client.println("<html>");
      
-    client.print((String)"<body><h1> Bem vindo"+user+"</h1><p>Status da porta: Aberta</p></body>");
-    
-    delay(3000);
+    client.print((String)"<body><h1> Bem vindo "+user+"</h1>");
+    client.print("<p>Status da porta:");
+    if(isDoorOpen){
+      client.print("Aberta"); 
+    }else{
+      client.print("Fechada");
+      }
+    client.print("</p>");
+    client.print(uid);
+    client.print("</p>");
+    client.print("</body>");
+     
   }  
 }
